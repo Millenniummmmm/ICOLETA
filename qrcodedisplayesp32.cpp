e45740204225c63930e714a0cc25e0d2382af3d7
Otávio Joshua <136638405+Millenniummmmm@users.noreply.github.com>
2024-10-08 21:57:33 -0300

    Update qrcodedisplayesp32.cpp

--- qrcodedisplayesp32.cpp
+++ qrcodedisplayesp32.cpp
@@ -3,7 +3,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-// CÓDIGO EM TESTE
+// CÓDIGO EM TESTE (ESP32)
 // Defina as dimensões do display OLED
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
